ff0bb4bab30038012ed5ba7fd1152b7a100fc755
Magnus Kingo <magnus@metteogivar.dk>
2024-03-04 11:13:38 +0100

    Added service folder and session.jsx to use cookies

---
+++ app/services/session.server.jsx
@@ -0,0 +1,15 @@
+import { createCookieSessionStorage } from "@remix-run/node";
+
+export const { getSession, commitSession, destroySession } =
+  createCookieSessionStorage({
+    cookie: {
+      name: "Eventplatform-session",
+      secrets: ["MadeByMeAndOnlyMe"],
+
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 30,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    },
+  });
