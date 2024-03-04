fb644becc70f68de77f559a1345d1187210a8cc9
Magnus Kingo <magnus@metteogivar.dk>
2024-03-04 11:16:43 +0100

    Changed amount of time before cookie expires

--- app/services/session.server.jsx
+++ app/services/session.server.jsx
@@ -8,7 +8,7 @@ export const { getSession, commitSession, destroySession } =
 
       sameSite: "lax",
       path: "/",
-      maxAge: 60 * 60 * 24 * 30,
+      maxAge: 60 * 60 * 6,
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
     },
