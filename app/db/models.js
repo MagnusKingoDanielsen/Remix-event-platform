e6b36efd0abf5ccf0aa3f47dfd2500bef2d7cd86
Magnus Kingo <magnus@metteogivar.dk>
2024-03-04 11:49:48 +0100

    Added userSchema and (basic and not done) signup

--- app/db/models.js
+++ app/db/models.js
@@ -37,7 +37,37 @@ const eventSchema = new Schema(
   // https://mongoosejs.com/docs/timestamps.html
   { timestamps: true },
 );
-
+const userSchema = new Schema(
+  {
+    date: {
+      type: Date,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    eventsJoined: {
+      type: Array,
+      required: false,
+    },
+    eventsCreated: {
+      type: Array,
+      required: false,
+    },
+  },
+  // Automatically add `createdAt` and `updatedAt` timestamps:
+  // https://mongoosejs.com/docs/timestamps.html
+  { timestamps: true },
+);
 // For each model you want to create, please define the model's name, the
 // associated schema (defined above), and the name of the associated collection
 // in the database (which will be created automatically).
@@ -46,5 +76,10 @@ export const models = [
     name: "Events",
     schema: eventSchema,
     collection: "events",
+  },
+  {
+    name: "Users",
+    schema: userSchema,
+    collection: "users",
   },
 ];
---
+++ app/routes/signup.jsx
@@ -0,0 +1,45 @@
+import { Form, redirect, useActionData } from "@remix-run/react";
+import { commitSession, getSession } from "../services/session.server.jsx";
+
+export async function loader({ request }) {
+  const session = await getSession(request.headers.get("Cookie"));
+  if (session.data.user) {
+    return redirect("/");
+  }
+  return session.data;
+}
+
+export default function LoginPage() {
+  const error = useActionData();
+  return (
+    <div className="signupContainer">
+      <Form method="post">
+        <input placeholder="email" name="email" type="email" required />
+        <input
+          placeholder="password"
+          name="password"
+          type="password"
+          required
+        />
+        {error && <p>{error}</p>}
+        <button>Create account</button>
+      </Form>
+    </div>
+  );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const { email, password } = Object.fromEntries(formData);
+
+  if (email === "test@test.dk" && password === "test") {
+    const session = await getSession();
+    session.set("user", true);
+
+    return redirect("/", {
+      headers: { "Set-Cookie": await commitSession(session) },
+    });
+  } else {
+    return "error", "Invalid email or password";
+  }
+}
