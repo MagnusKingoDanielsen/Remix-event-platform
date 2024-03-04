30f056f3c317d54fbdfc3de70dc9e29ca4393aa8
Magnus Kingo <magnus@metteogivar.dk>
2024-03-04 12:04:12 +0100

    Added logout button to index

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,14 +1,37 @@
-import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, redirect } from "@remix-run/react";
+import { getSession, destroySession } from "../services/session.server.jsx";
+import { Form, Link } from "@remix-run/react";
 import mongoose from "mongoose";
 
-export async function loader() {
+export async function loader({ request }) {
+  const session = await getSession(request.headers.get("cookie"));
   const entries = await mongoose.models.Events.find({});
-  return json({ entries });
+
+  return { session: session.data };
 }
 
 export default function Index() {
-  const { entries } = useLoaderData();
+  const { entries, session } = useLoaderData();
 
-  return <div className=""></div>;
+  return (
+    <div className="">
+      {session.user ? (
+        <Form method="post">
+          <button className="logoutBTN">Logout</button>
+        </Form>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
+    </div>
+  );
+}
+
+export async function action({ request }) {
+  const session = await getSession(request.headers.get("cookie"));
+
+  return redirect("/", {
+    headers: {
+      "Set-Cookie": await destroySession(session),
+    },
+  });
 }
