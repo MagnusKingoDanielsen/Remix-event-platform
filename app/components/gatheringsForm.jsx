fe5d6c3db74962cd630cac1a5a3d87cdf6416aaf
Magnus Kingo <magnus@metteogivar.dk>
2024-03-05 14:33:18 +0100

    Added create gatherings, Images and changed model
    With firebase as a bucket for images

--- app/components/gatheringsForm.jsx
+++ app/components/gatheringsForm.jsx
@@ -1,7 +1,8 @@
 import { useFetcher } from "@remix-run/react";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export default function PostForm({ post }) {
+export default function gatheringsForm({ post = {} }) {
+  const [image, setImage] = useState(post?.imageUrl ? post?.imageUrl : null);
   const fetcher = useFetcher();
   const descriptionareaRef = useRef(null);
   const isIdle = fetcher.state === "idle";
@@ -9,6 +10,21 @@ export default function PostForm({ post }) {
   const dateValue = post.date
     ? new Date(post.date).toISOString().split("T")[0]
     : "";
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (file.size < 1000000) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setImage(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      alert("Image must be less than 1MB in size.");
+      event.target.value = "";
+    }
+  }
+
   // Clear the form and focus the descriptionarea after a submission
   useEffect(() => {
     if (!isInit && isIdle && descriptionareaRef.current) {
@@ -62,6 +78,28 @@ export default function PostForm({ post }) {
               defaultValue={post?.endTime}
             />
           </div>
+        </div>
+        <div className="place">
+          <input
+            type="text"
+            id="place"
+            name="place"
+            placeholder="Place"
+            required
+            defaultValue={post?.place}
+          />
+        </div>
+        <div className="">
+          <label className="" htmlFor="file_input">
+            Image
+          </label>
+          <input
+            className=""
+            name="image"
+            type="file"
+            onChange={handleImageChange}
+          />
+          {image && <img src={image} alt="" className="" width={400} />}
         </div>
         <div className="description">
           <textarea
