c68d852d83ebda588550a897d2e3cf38a6486609
Magnus Kingo <magnus@metteogivar.dk>
2024-03-07 14:10:50 +0100

    Added link to edit page

--- app/routes/myGatherings.jsx
+++ app/routes/myGatherings.jsx
@@ -1,4 +1,4 @@
-import { redirect, useLoaderData } from "@remix-run/react";
+import { redirect, useLoaderData, Link } from "@remix-run/react";
 import { getSession } from "../services/session.server.jsx";
 import mongoose from "mongoose";
 import attendingImg from "../img/attending.png";
@@ -35,38 +35,43 @@ export default function MyGatherings() {
       <div className="displayGatherings">
         {gatherings.map((gathering) => (
           <div key={gathering._id} className="gatheringCard">
-            <div className="gatheringCardHeader">
-              <img src={gathering.imageUrl} alt="gathering image" />
-              <div className="gatheringCardHeaderInfo">
-                <div className="gatheringCardHeaderInfoText">
-                  <span>{gathering.date}</span>
-                  <span>
-                    {gathering.attending.length}
-                    <img src={attendingImg} alt="Attending img" />
-                    <br />
-                    attending
+            <Link to={`/gathering/${gathering._id}`}>
+              <div className="gatheringCardHeader">
+                <img src={gathering.imageUrl} alt="gathering image" />
+                <div className="gatheringCardHeaderInfo">
+                  <div className="gatheringCardHeaderInfoText">
+                    <span>{gathering.date}</span>
+                    <span>
+                      {gathering.attending.length}
+                      <img src={attendingImg} alt="Attending img" />
+                      <br />
+                      attending
+                    </span>
+                  </div>
+                  <div className="gatheringCardHeaderInfoPips">
+                    <div className="pip" />
+                  </div>
+                </div>
+              </div>
+              <div className="gatheringCardContent">
+                <h2 className="gatheringTitle">{gathering.title}</h2>
+                <p className="gatheringDescription">{gathering.description}</p>
+                <div className="gatheringLocation">
+                  <span className="gatheringAt">Location:</span>
+                  <span className="gatheringPlace">{gathering.place}</span>
+                </div>
+                <div className="gatheringTimes">
+                  <span className="gatheringStartTime">
+                    {gathering.startTime}
                   </span>
+                  <span className="gatheringTimeSeparator"> from </span>
+                  <span className="gatheringEndTime">{gathering.endTime}</span>
                 </div>
-                <div className="gatheringCardHeaderInfoPips">
-                  <div className="pip" />
-                </div>
+                <Link to={`/myGathering/${gathering._id}/edit`}>
+                  <button className="editButton">Edit</button>
+                </Link>
               </div>
-            </div>
-            <div className="gatheringCardContent">
-              <h2 className="gatheringTitle">{gathering.title}</h2>
-              <p className="gatheringDescription">{gathering.description}</p>
-              <div className="gatheringLocation">
-                <span className="gatheringAt">Location:</span>
-                <span className="gatheringPlace">{gathering.place}</span>
-              </div>
-              <div className="gatheringTimes">
-                <span className="gatheringStartTime">
-                  {gathering.startTime}
-                </span>
-                <span className="gatheringTimeSeparator"> from </span>
-                <span className="gatheringEndTime">{gathering.endTime}</span>
-              </div>
-            </div>
+            </Link>
           </div>
         ))}
       </div>
